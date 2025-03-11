0845f328ba97ebf198e5273dfa1740aec3e8ac2e
fabiomode2 <fabiomancebotrillo35@gmail.com>
2025-03-11 23:24:58 +0100

    da

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+const icons = [
+  "setting",
+  "cloudo",
+  "eyeo",
+  "delete",
+  "folder1",
+  "file1",
+  "addfolder",
+  "folderopen",
+  "search1",
+  "calendar",
+  "filetext",
+  "sync",
+  "reload1",
+  "warning",
+  "home",
+  "menuunfold",
+  "cloudupload",
+];
+
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -21,16 +43,29 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}>
+      }}
+    >
+      <Tabs.Screen
+        name="archivo"
+        options={{
+          title: "Archivo",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="folder1" size={24} color={color} />
+          ),
+        }}
+      />
+
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Inicio",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -38,7 +73,7 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -48,10 +83,12 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="grabar"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: "Añadir audio",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="plus" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Link } from "expo-router";
 
-export default function TabOneScreen() {
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+export default function index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.hcontainer}>
+        <Link href="/grabar">
+          <AntDesign name="plus" size={24} />
+          <Text style={styles.text_medium}> Subir audio </Text>
+        </Link>
+
+        <Link href="/archivo">
+          <AntDesign name="folder1" size={24} />
+          <Text style={styles.text_medium}> Ver archivo </Text>
+        </Link>
+      </View>
+
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <AntDesign name="calendar" size={32} />
     </View>
   );
 }
@@ -16,16 +34,57 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
+  hcontainer: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 20,
+    padding: 20,
+    maxHeight: "10%",
+    position: "absolute",
+    top: 0,
+  },
+
+  button1: {
+    width: "40%", // Aproximadamente la mitad de la pantalla con márgenes
+    aspectRatio: 1, // Hace que sea un cuadrado
+    backgroundColor: "#4CAF50",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  text: {
+    fontSize: 12,
+    textAlign: "center",
+  },
+  text_medium: {
+    fontSize: 18,
+    textAlign: "center",
+    fontFamily: "Ubuntu-Regular",
+  },
+  topView: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    width: "100%",
+    padding: 10,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
